62cf8212363edb9f95968c0513f080bfea726726
Eric Hyde <hydestuf@gmail.com>
2021-02-05 18:52:48 -0600

    Seed script working. building database script

--- dataGen/csvgen.js
+++ dataGen/csvgen.js
@@ -56,3 +56,31 @@ const write = async (fileName, fields, data, qty) => {
     recordsToGenerate--;
   }
 
+
+// INSERT INTO sdcproducts (
+  // product_id,
+  // name,
+  // image_url,
+  // price,
+  // description,
+  // category_id
+// ) VALUES (
+// 1,
+// 'awesome jacket',
+// '/Users/erichyde/HR/sdc/GAR-FEC-SimilarProducts/databasePg/images/endOfInternet.jpg',
+// 79.99,
+// 'cyber cat spinning out the vibes',
+// 'jacket'
+// );
+
+// https://www.youtube.com/watch?v=qw--VYLpxG4
+// 36:50
+
+// create table sdcproducts(
+//   product_id SERIAL PRIMARY KEY,
+//   name VARCHAR(60) NOT NULL,
+//   image_url varchar(250) NOT NULL,
+//   price integer NOT NULL,
+//   description varchar(250) NOT NULL,
+//   category_id integer NOT NULL);
+
